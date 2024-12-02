67e0843c3f865b827b86b697521ff2e2d577baeb
David Salazar <davidsalazar200@gmail.com>
2024-12-02 10:12:30 -0500

    Applied lint

--- client/src/components/ChartSelector.jsx
+++ client/src/components/ChartSelector.jsx
@@ -1,12 +1,12 @@
 const ChartSelector = ({ availableCharts, selectedCharts, onSelectChart }) => {
   return (
-    <div className="my-4">
-      <div className="flex flex-wrap gap-2">
+    <div className='my-4'>
+      <div className='flex flex-wrap gap-2'>
         {availableCharts.map((chart) => (
           <button
             key={chart.id}
             className={`btn btn-sm ${
-              selectedCharts.includes(chart.id) ? "btn-primary text-white" : "btn-outline"
+              selectedCharts.includes(chart.id) ? 'btn-primary text-white' : 'btn-outline'
             }`}
             onClick={() => onSelectChart(chart.id)}
           >
@@ -15,7 +15,7 @@ const ChartSelector = ({ availableCharts, selectedCharts, onSelectChart }) => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ChartSelector;
+export default ChartSelector
